a18b7f168eeb316ed5a728993cd50dd6c109ea32
Florian Beeres <yuuki@protonmail.com>
2020-07-19 14:59:58 +0200

    Add search through fzf and ripgrep

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # Fish Notes :fish:
 
-## Disclaimer
-
-I created this primarily for myself. It's therefore very minimal and requires setting up your own mappings, as shown in `contrib/scripts.fish`. The only thing this little script does is let you easily add notes which are stored in a way that combines human readable text with a bit of structure for easier analysis.
-
 ## Usage
+
+### Create Note
 
 ```text
 $ set -x FISH_NOTES_DIR ~/.local/share/fish_notes
@@ -12,8 +10,7 @@ $ notes new -T "This is a title" -t these -t are -t tags
 Created new note '/home/tifa/.local/share/fish_notes/616627'
 ```
 
-That's it! There is no search or edit functionality. Check the `contrib/`
-folder for ideas. Your notes will be stored like this:
+Your notes will be stored like this:
 
 ```
 fish_notes λ tree
@@ -30,6 +27,10 @@ fish_notes λ tree
 │   └── title
 ```
 
+### Search Notes
+
+You can search notes by tags and by content with the `notes search_content` and `notes search_tags` commands. Both will spawn a FZF shell where you can interactively narrow down your search.
+
 ## Options
 
 - `FISH_NOTES_EXTENSION`: Default file extension for note content
@@ -38,3 +39,8 @@ fish_notes λ tree
   ```fish
   $ function __notes_entry_template; echo "foo"; end; notes new
   ```
+
+## Disclaimer
+
+I created this primarily for myself. It's therefore quite minimal and requires both Ripgrep and FZF, in case you want to use search. Additionally, I can't guarantee that all features work with all symbols and characters.
+
--- functions/notes.fish
+++ functions/notes.fish
@@ -147,23 +147,122 @@ function notes -a cmd -d "Fish Notes"
         case titles
             __notes_list_titles
         case new
-            # TODO: Add help for new
-            # if set -q _flag_h
-            #     __notes_help_new
-            #     return 0
-            # end
-
             set -e argv[1]
             __notes_new $argv
+        case search_content
+            __notes_by_content
+        case search_tags
+            __notes_by_tags
         case \*
             set -e argv[1]
             __notes_help
     end
 end
 
+# This is the function used for the --preview argument when previewing tag
+# search results. See 'man fzf' for details, but here's a quick summary:
+# > Use {+n} if you want all index numbers when multiple lines are selected
+# Careful with quoting in here, it's easy to overlook that this is a string.
+# Especially single quotes can be tricky.
+set __tags_preview_func_string '\
+    if test (count {2..}) -eq 0
+        return
+    end
+
+    set -l tag_results $FISH_NOTES_DIR/*/tags
+
+    for tag in {2..}
+        set tag_results (rg --files-with-matches $tag $tag_results)
+    end
+
+    for r in $tag_results
+        set -l dn (dirname $r)
+        printf "Title: %s\n" (cat $dn/title)
+        printf "Tags: %s\n" (cat $dn/tags | string join " ")
+        cat $dn/body*
+    end\
+'
+
+function __notes_by_tags
+    if not type -q rg
+        echo 'Ripgrep is required but not found'
+    end
+
+    if not type -q fzf
+        echo 'FZF is required but not found'
+    end
+
+    # Gather all tags and make them sorted and unique
+    set -l tags (rg '.*' $FISH_NOTES_DIR/*/tags | sed '/^$/d' | sort | uniq | string collect)
+
+    # This stores the filenames that the user chose through FZF
+    set -l results (                                  \
+         echo $tags | fzf                             \
+                --delimiter ':'                       \
+                --preview $__tags_preview_func_string \
+                --multi                               \
+                --with-nth 2..                        \
+                --preview-window down:wrap            \
+    )
+
+    for v in $results
+        set -l dir (dirname $v)
+
+        printf "%s\n" (string repeat -n 80 "-")
+
+        printf "Title: %s\n" (cat $dir/title)
+        printf "Tags: %s\n" (string join " " < $dir/tags)
+        printf "Date: %s\n" (cat $dir/date)
+        fold -s $dir/body*
+    end
+end
+
+function __notes_by_content
+    if not type -q rg
+        echo 'Ripgrep is required but not found'
+    end
+
+    if not type -q fzf
+        echo 'FZF is required but not found'
+    end
+
+    set -l result (\
+        rg '.*' $FISH_NOTES_DIR/**/body.md          \
+            | sed '/:$/d'                           \
+            | fzf --height 70%                      \
+                --preview 'cat (dirname {1})/body*' \
+                --delimiter ':'                     \
+                --with-nth 2..                      \
+                --preview-window down:wrap)
+
+    if not test -n "$result"
+        return
+    end
+
+    set -l dir (dirname $result)
+
+    printf "%s\n" (string repeat -n 80 "-")
+
+    printf "Title: %s\n" (cat $dir/title)
+    printf "Tags: %s\n" (string join " " < $dir/tags)
+    printf "Date: %s\n" (cat $dir/date)
+    fold -s $dir/body*
+end
+
 function __notes_help
     echo 'Usage:'
     echo 'notes help/--help/-h     Show this help'
+    echo ''
+    echo 'notes search_tags        Search through notes by tags'
+    echo '                         Requires ripgrep and FZF'
+    echo ''
+    echo 'notes search_content     Search through notes by their content'
+    echo '                         Requires ripgrep and FZF'
+    echo ''
+    echo 'notes new                Create a new note'
+    echo '      -t/--tag   TAG     Can be passed multiple times'
+    echo '                         Each passed value will be one tag of the new entry'
+    echo '      -T/--title TITLE   Title for new note'
     echo ''
     echo 'Customziations:'
     echo 'You can customize the default template, meaning the text that will be'
