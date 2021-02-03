a097457c7c7225e38c22f26101217881adad2553
Florian Beeres <yuuki@protonmail.com>
2021-02-03 11:15:10 +0100

    Add completion for existing tags and titles

--- completions/notes.fish
+++ completions/notes.fish
@@ -1,4 +1,3 @@
-
 # Disable file completion
 complete -c notes -f
 
@@ -9,5 +8,6 @@ complete -c notes -n "not __fish_seen_subcommand_from $commands" -a "new" -d "cr
 complete -c notes -n "not __fish_seen_subcommand_from $commands" -a "help" -d "show help and usage" 
 
 # Switch/options completion
-complete -c notes -s h -l help            -d "Help"
-
+complete -c notes -s h -l help                                                 -d "Help"
+complete -c notes -s t -l tags            -n "__fish_seen_subcommand_from new" -d "Tags" -a "(notes tags)"   -r -f
+complete -c notes -s T -l title           -n "__fish_seen_subcommand_from new" -d "Title" -a "(notes titles)"  -r -f
